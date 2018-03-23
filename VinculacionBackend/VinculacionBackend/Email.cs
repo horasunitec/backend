d8f21c80ee537aa0d4f619e157676600ffce2a7d
horasunitec <horasunitec@gmail.com>
2018-03-22 20:47:58 -0600

    B4 - Mailgun Server not sending emails fix

--- VinculacionBackend/VinculacionBackend/Email.cs
+++ VinculacionBackend/VinculacionBackend/Email.cs
@@ -10,7 +10,7 @@ namespace VinculacionBackend
         public  IRestResponse Send(string emailAdress, string message, string subject)
         {
             RestClient client = new RestClient();
-            client.BaseUrl = "https://api.mailgun.net/v3";
+            client.BaseUrl = new System.Uri("https://api.mailgun.net/v3");
             client.Authenticator =
             new HttpBasicAuthenticator("api",
                                       "key-90c09be8abf5f2eca6588d944727a22b");
@@ -19,7 +19,7 @@ namespace VinculacionBackend
             request.Resource = "{domain}/messages";
             //request.AddParameter("from", "Mailgun Sandbox <postmaster@sandbox792e3910e58e449d870d909b6e71e032.mailgun.org>");
             request.AddParameter("from", "Sistema Horas Vinculacion UNITEC <horasunitec@gmail.com>");
-            request.AddParameter("to", emailAddress);
+            request.AddParameter("to", emailAdress);
             request.AddParameter("subject", subject);
             request.AddParameter("text", message);
             request.Method = Method.POST;
