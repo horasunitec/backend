48462cddfe1b061c6bd91bc24b0d34dd0e8d7da7
horasunitec <horasunitec@gmail.com>
2018-03-22 20:43:45 -0600

    B4 - Mailgun Server not sending emails

--- VinculacionBackend/VinculacionBackend/Email.cs
+++ VinculacionBackend/VinculacionBackend/Email.cs
@@ -9,19 +9,17 @@ namespace VinculacionBackend
     {    
         public  IRestResponse Send(string emailAdress, string message, string subject)
         {
-            RestClient client = new RestClient
-            {
-                BaseUrl = new Uri("https://api.mailgun.net/v3"),
-                Authenticator = new HttpBasicAuthenticator("api",
-                    "key-8tw489mxfegaqewx93in2xo449q5p3l0")
-            };
-            var request = new RestRequest();
-            request.AddParameter("domain",
-                "app5dcaf6d377cc4ddcb696b827eabcb975.mailgun.org", ParameterType.UrlSegment);
+            RestClient client = new RestClient();
+            client.BaseUrl = "https://api.mailgun.net/v3";
+            client.Authenticator =
+            new HttpBasicAuthenticator("api",
+                                      "key-90c09be8abf5f2eca6588d944727a22b");
+            RestRequest request = new RestRequest();
+            request.AddParameter("domain", "sandbox792e3910e58e449d870d909b6e71e032.mailgun.org", ParameterType.UrlSegment);
             request.Resource = "{domain}/messages";
-            request.AddParameter("from", "vinculacion@gmail.com");
-            String email = emailAdress;
-            request.AddParameter("to", email);
+            //request.AddParameter("from", "Mailgun Sandbox <postmaster@sandbox792e3910e58e449d870d909b6e71e032.mailgun.org>");
+            request.AddParameter("from", "Sistema Horas Vinculacion UNITEC <horasunitec@gmail.com>");
+            request.AddParameter("to", emailAddress);
             request.AddParameter("subject", subject);
             request.AddParameter("text", message);
             request.Method = Method.POST;
