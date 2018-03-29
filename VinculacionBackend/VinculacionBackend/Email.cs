da4bba7da88a52a0d42f21f42aaf20f7647fbaa4
horasunitec <horasunitec@gmail.com>
2018-03-29 00:01:57 -0600

    Send Email class fix

--- VinculacionBackend/VinculacionBackend/Email.cs
+++ VinculacionBackend/VinculacionBackend/Email.cs
@@ -7,39 +7,28 @@ namespace VinculacionBackend
 {
     public class Email:IEmail
     {
-        private static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
         public void Send(string emailAdress, string msg, string subject)
         {
-            try
-            {
-                var fromAddress = new MailAddress("horasunitec@gmail.com");
-                var fromPassword = "goldeneye007";
-                var toAddress = new MailAddress(emailAdress);
+            var fromAddress = new MailAddress("horasunitec@gmail.com");
+            var fromPassword = "goldeneye007";
+            var toAddress = new MailAddress(emailAdress);
 
-                System.Net.Mail.SmtpClient smtp = new System.Net.Mail.SmtpClient
-                {
-                    Host = "smtp.gmail.com",
-                    Port = 587,
-                    EnableSsl = true,
-                    DeliveryMethod = SmtpDeliveryMethod.Network,
-                    UseDefaultCredentials = false,
-                    Credentials = new NetworkCredential(fromAddress.Address, fromPassword)
-                };
+            System.Net.Mail.SmtpClient smtp = new System.Net.Mail.SmtpClient
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(fromAddress.Address, fromPassword)
+            };
 
-                using (var message = new MailMessage(fromAddress, toAddress)
-                {
-                    Subject = subject,
-                    Body = msg
-                }) ;
-            }
-            catch (Exception e)
+            var message = new MailMessage(fromAddress, toAddress)
             {
-                logger.Error(e.Message);
-            }
-            finally
-            {
-                logger.Info("Finally");
-            }
+                Subject = subject,
+                Body = msg
+            };
+            smtp.Send(message);
         }
     }
 }
